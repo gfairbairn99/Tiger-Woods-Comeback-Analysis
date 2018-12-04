afb52e993bc73e02c002de1ab1a165d1d1c0e4a0
Grant Fairbairn <grantfairbairn@Grants-MacBook-Pro.local>
2018-12-03 23:27:11 -0500

    cleaned up second and third tabs of my shiny app, going to go back and reconsider my first tab. Added double filter for tab 3! Things for next push: Comment code, add interesting things to note, explain what I am vizualing, better names and labels.

--- fp_app/app.R
+++ fp_app/app.R
@@ -15,58 +15,120 @@ library(readxl)
 library(janitor)
 library(plotly)
 
-only_tiger <- read_excel("only_tiger.xlsx") %>% 
-  clean_names()
+tiger <- readRDS("~/fp_fairbairn/fp_app/fp.rds")
 
-ui <- fluidPage(
-                
-   titlePanel("Tiger"),
+#tiger <- read_excel("only_tiger.xlsx") %>% 
+#clean_names()
 
-   sidebarLayout(
-     sidebarPanel(
-       checkboxGroupInput("tournament",
-                   "Tournament Name",
-                   choices = c("Farmers Insurance Open", "Genesis Open",
-                               "The Honda Classic", "Valspar Championship",
-                               "Arnold Palmer Invitational presented by Mastercard",
-                               "Wells Fargo Championship", "THE PLAYERS Championship",
-                               "the Memorial Tournament presented by Nationwide",
-                               "Quicken Loans National", "World Golf Championships-Bridgestone Invitational",
-                               "PGA Championship", "THE NORTHERN TRUST", "Dell Technologies Championship",
-                               "BMW Championship", "TOUR Championship"
-                               ),
-                   selected = c("Farmers Insurance Open"))
-    
-     ),
       
-      # Show a plot of the generated distribution
-      mainPanel(
-        tabsetPanel(type = "tabs",
-                    tabPanel("1", plotOutput("distPlot")),
-                    tabPanel("2"),
-                    tabPanel("3")
-                    )
-      )
-   )
-)
 
+
+ui <- navbarPage("My Application",
+             tabPanel("Component 1",
+               sidebarLayout(
+                 sidebarPanel(
+                   checkboxGroupInput("tournament",
+                                      "Tournament Name",
+                                      choices = c("Farmers Insurance Open", "Genesis Open",
+                                                  "The Honda Classic", "Valspar Championship",
+                                                  "Arnold Palmer Invitational presented by Mastercard",
+                                                  "Wells Fargo Championship", "THE PLAYERS Championship",
+                                                  "the Memorial Tournament presented by Nationwide",
+                                                  "Quicken Loans National", "World Golf Championships-Bridgestone Invitational",
+                                                  "PGA Championship", "THE NORTHERN TRUST", "Dell Technologies Championship",
+                                                  "BMW Championship", "TOUR Championship"),
+                                      selected = c("Farmers Insurance Open"))
+                  ),
+                 mainPanel(plotOutput("strokes_gained"))
+               )
+             ),
+             
+             
+             tabPanel("Component 2",
+                      sidebarLayout(
+                        sidebarPanel(
+                          #sliderInput()
+                        ),
+                        mainPanel(
+                          plotOutput("yardage")
+                          )
+                      )
+             ),
+
+             tabPanel("Component 3",
+                      sidebarLayout(
+                        sidebarPanel(
+                          sliderInput("range", "Range:",
+                                      min = 125, max = 300,
+                                      value = c(150,175)
+                                      ), 
+                          selectInput("par_value",
+                                             "Par Value",
+                                             choices = c("3", "4", "5"),
+                                             selected = c("3")
+                          )
+                        ),
+                          mainPanel(
+                            tableOutput("tiger_table")
+                          )
+                        )
+                      )
+             )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-     only_tiger %>% 
-       filter(tournament_name == input$tournament) %>% 
-       
-       ggplot(aes(x=shot, y=strokes_gained_baseline, color = tournament_name))+
-       geom_point()+
-       theme_classic()+
-       xlab("Shot")+
-       ylab("Strokes Gained")+
-       scale_colour_discrete(name="Tournament Name")+
-       xlim(0,8)+
-       ylim(-2,2)
-   })
+  
+  output$strokes_gained <- renderPlot({
+    tiger %>% 
+      filter(tournament_name == input$tournament) %>% 
+      ggplot(aes(x=shot, y=strokes_gained_baseline, color = tournament_name))+
+      geom_point()+
+      theme_classic()+
+      xlab("Shot")+
+      ylab("Strokes Gained")+
+      scale_colour_discrete(name="Tournament Name")+
+      xlim(0,8)+
+      ylim(-2,2)
+  })
+  output$yardage <- renderPlot({
+    tiger %>% 
+      group_by(tournament_name, ez_date) %>% 
+      summarize(scoring_avg = NROW(shot)/4) %>% 
+      arrange(ez_date)%>% 
+      filter(tournament_name != "Genesis Open")%>% 
+      ggplot(aes(x=ez_date, y=scoring_avg))+
+      geom_point()+
+      geom_smooth(method = "lm") %>% 
+      add_trace(
+        text = c("text A"),
+        hoverinfo = `text`)
+  })
+    output$tiger_table <- renderTable({
+      # tiger %>%
+      #   filter(proximity_yardage %in% input$range) %>%
+      #   group_by(tournament_name, date) %>%
+      #   summarize(scoring_avg = NROW(shot)) %>%
+      #   select(tournament_name, place, date) %>%
+      #   filter(! is.na(proximity_range)) %>%
+      #   arrange(proximity_range)
+      
+      tiger %>% 
+        filter(recovery_shot == "No") %>% 
+        mutate(keep = case_when(par_value == 3 & shot== 1 ~ "keep",
+                                par_value == 4 & shot == 2 ~ "keep",
+                                par_value == 5 & shot == 3 ~ "keep",
+                                TRUE ~ "delete")) %>% 
+        filter(keep=="keep") %>% 
+        filter(par_value %in% input$par_value) %>% 
+        filter(from_pin_yardage >= input$range[1],
+               from_pin_yardage <= input$range[2]) %>% 
+        select(tournament_name, hole, feet_to_pin_after_shot, from_pin_yardage) %>% 
+        arrange(feet_to_pin_after_shot) %>% 
+        filter(feet_to_pin_after_shot != 0)
+
+        
+      
+ })
 }
 
 # Run the application 
